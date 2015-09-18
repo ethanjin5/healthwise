e7fb8d09f8592dd9c45902f43ad4fac93144662e
feifan-agillapro <feifansnf@gmail.com>
2015-09-18 14:35:25 -0400

    date for appointment,added firebase

--- app/components/appointment/appointment.js
+++ app/components/appointment/appointment.js
@@ -1,6 +1,6 @@
 
 'use strict';
-angular.module('appointment',['ngRoute'])
+angular.module('appointment',['ngRoute','firebase'])
 .config(function($routeProvider, $locationProvider) {
   $routeProvider
   .when('/appointment/makeappointment', {
@@ -11,6 +11,9 @@ angular.module('appointment',['ngRoute'])
   $locationProvider.html5Mode(true);
 })
 
-.controller('AppointmentCtrl', function ($scope, $routeParams) {
-console.log( $routeParams);
+.controller('AppointmentCtrl', function ($scope, $routeParams, $firebase) {
+	var ref = new Firebase('https://radiant-fire-8311.firebaseio.com/Appointment');
+	var fb = $firebase(ref);
+
+	$scope.date = new Date();
 });
